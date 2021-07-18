cd06e88d122a4cb11978b4e28d1bfdbdd9c79e24
Andrew Borgan <36088227+aborgan@users.noreply.github.com>
2021-07-18 10:40:10 -0400

    create typography component + example (#4)

---
+++ src/examples/Typography.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Typography } from '../Typography';
+
+export const TypographyExample: React.FC<{}> = () => (
+  <div>
+    <Typography>This is a test</Typography>
+  </div>
+);
